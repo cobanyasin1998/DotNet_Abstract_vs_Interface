f36e2d620d89df2143097d8c296c8f79eb2abed1
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-25 23:51:47 +0300

    Add project files.

---
+++ DotNet_Abstract_vs_Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet_Abstract_vs_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
