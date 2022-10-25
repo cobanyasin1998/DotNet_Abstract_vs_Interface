385e666840a00ea87df96f8cecf025772c52c2e8
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-26 00:07:22 +0300

    base class

--- DotNet_Abstract_vs_Interface/DatabaseTestMethods.cs
+++ DotNet_Abstract_vs_Interface/DatabaseTestMethods.cs
@@ -1,7 +1,31 @@
-﻿namespace DotNet_Abstract_vs_Interface
-{
-    class DatabaseTestMethods
-    {
+﻿using System.Data;
 
+namespace DotNet_Abstract_vs_Interface
+{
+
+    public class BaseDb
+    {
+        public DataTable ExecuteSql(string sql)
+        {
+            return new DataTable();
+        }
+    }
+
+    public class SqlServerDb:BaseDb
+    {
+        public string GetDbVersion() => "SqlServer 2012";
+
+        public string GenerateSql(int id)
+        {
+            return $"SELECT * FROM USERS WITH(NOLOCK) WHERE ID = {id}";
+        }
+    }
+    public class OracleDb : BaseDb
+    {
+        public string GetDbVersion() => "Oracle 18c";
+        public string GenerateSql(int id)
+        {
+            return $"SELECT * FROM USERS WHERE USER_ID = {id}";
+        }
     }
 }
